ec29b9a3487e869e25178556843aba7fbd97b279
Filip Van der Schueren <filipvanderschueren@users.noreply.github.com>
2019-03-02 22:05:32 +0100

    Update Main.java
    message changed

--- src/be/uantwerpen/fti/Main.java
+++ src/be/uantwerpen/fti/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args)
     {
 	// write your code here
-        System.out.printf( "hello message..." );
+        System.out.printf( "hello changed message..." );
     }
 }
