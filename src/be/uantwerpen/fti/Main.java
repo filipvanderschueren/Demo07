40a5e936b13a4e06c314128bd24d53fe5fd0db3a
Filip Van der Schueren <filipvanderschueren@users.noreply.github.com>
2019-03-02 22:15:37 +0100

    Update Main.java
    message changed

--- src/be/uantwerpen/fti/Main.java
+++ src/be/uantwerpen/fti/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	// write your code here
         System.out.printf( "hello changed message..." );
         aClass m = new aClass();
-        m.setMessage( "newMessage..." );
+        m.setMessage( "newMessage...from branch01 edited" );
         System.out.printf( "and now from the Class : " + m.getMessage() );
     }
 }
