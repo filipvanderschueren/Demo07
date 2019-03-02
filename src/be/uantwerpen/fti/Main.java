2d46a520da427e98ecdd6f1b4e07da3d2dfbd52d
Filip Van der Schueren <filip.vanderschueren@uantwerpen.be>
2019-03-02 22:33:36 +0100

    new bClass, and main changed

--- src/be/uantwerpen/fti/Main.java
+++ src/be/uantwerpen/fti/Main.java
@@ -5,9 +5,13 @@ public class Main {
     public static void main(String[] args)
     {
 	// write your code here
-        System.out.printf( "hello changed message..." );
+        System.out.println( "hello changed message..." );
         aClass m = new aClass();
         m.setMessage( "newMessage...from branch01 edited" );
-        System.out.printf( "and now from the Class : " + m.getMessage() );
+        System.out.println( "and now from the Class : " + m.getMessage() );
+
+        bClass b = new bClass( 7 );
+        System.out.println( "and now a value from bClass :" + b.getValue());
+
     }
 }
---
+++ src/be/uantwerpen/fti/bClass.java
@@ -0,0 +1,17 @@
+package be.uantwerpen.fti;
+
+public class bClass {
+    private int value;
+
+    public bClass(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
